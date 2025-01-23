e5c83b613566bd453b78aebb006c590e40d9950e
Burn1ngShade <92182982+Burn1ngShade@users.noreply.github.com>
2025-01-23 20:10:52 +0000

    yo

--- obj/Debug/net8.0/Revistone.AssemblyInfo.cs
+++ obj/Debug/net8.0/Revistone.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Revistone")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2c0ddbf817fd309fea75aea860abb6f0b118690e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6bf0bc6273de67f7c12ae7d388b8f3143f0d4aea")]
 [assembly: System.Reflection.AssemblyProductAttribute("Revistone")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Revistone")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
